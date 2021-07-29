35e6cbfd620dc8a2a3eba5631949bfb8e1cab8bd
Angelica Sargent <agraysargent@gmail.com>
2021-07-29 15:53:08 -0400

    api nearly finished

---
+++ routes/api.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workout"),
+  ({ body }, res) => {
+    Workout.create(body)
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  };
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  console.log(body);
+  Workout.findByIDAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workout/range", (req, res) => {
+  Workout.find({})
+    .limit(10)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.delete("/api/workout", ({ body }, res) => {
+  Workout.findByIdAndRemove(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
